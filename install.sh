ecd0b1f7bf3fb54d23690a7d5a7032e0bddec1cd
Fusion-Z <51750215+Fusion-Z@users.noreply.github.com>
2020-05-17 18:48:05 +0930

    Update install.sh

--- install.sh
+++ install.sh
@@ -84,7 +84,7 @@ msg "Getting URL for latest Home Assistant disk image..."
 RELEASE_EXT=vmdk.gz
 URL=$(cat<<EOF | python3
 import requests
-url = 'https://github.com/home-assistant/operating-system/releases/tag/3.6'
+url = 'https://github.com/home-assistant/operating-system/releases/download/3.6'
 r = requests.get(url).json()
 if 'message' in r:
   exit()
